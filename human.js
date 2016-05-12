c5b98a5c8e9b790e0b0d330989cbf79e490c52a6
Valentina <v.kozlova13@gmail.com>
2016-05-12 23:58:01 +0300

    Чистка говнокода. Часть 1. Главный герой сумел превести все элементы LESS в порядок. Но он не проверял, как они устроены внутри. И правильно сделал, иначе пришлось бы лечиться вином еще неделю после этого.
    То, что отвечает за пейзажи переселили в отдельный файл.
    Девочка очень просит сделать ей пластику носа и новую прическу. Поэтому в следующих частях ГГ обязательно ее спасет.
    
    Но наш ГГ столкнулся с другой проблемой. В букете цветов девочки просто адский говнокод, которому поможет лишь javascript. ГГ не знает, что ему делать в первую очередь, спасать девочку или ее букет. Об этом вы сможете почитать в следующих частях. Приятного просмотра.
    
    Продолжение следует...

---
+++ human.js
@@ -0,0 +1,15 @@
+function Human() {
+	return this;
+}
+
+Human.prototype.generatePerson = function() {
+	
+};
+
+Human.prototype.generateFlowers = function() {
+	
+};
+
+Human.prototype.generateFlower = function() {
+	
+}
